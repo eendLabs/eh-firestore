c523d2824428ef93b0c37ba2eb8ca635aab09cfb
Erick Gomez <erickg.gomez@gmail.com>
2021-02-12 12:50:43 -0500

    test: add test for event store

---
+++ pkg/eventstore/eventstore_test.go
@@ -0,0 +1,44 @@
+package eventstore
+
+import (
+	"context"
+	eh "github.com/looplab/eventhorizon"
+	testutil "github.com/looplab/eventhorizon/eventstore"
+	"testing"
+)
+
+func TestEventStore(t *testing.T) {
+	ctx := context.Background()
+	store, err := NewEventStore(&EventStoreConfig{
+		projectID:  "dummy-project-id",
+		collection: "test",
+	})
+
+	if err != nil {
+		t.Fatal("there should be no error:", err)
+	}
+	if store == nil {
+		t.Fatal("there should be a store")
+	}
+
+	ctx = eh.NewContextWithNamespace(context.Background(), "ns")
+	defer func() {
+		t.Log("clearing db")
+		if err = store.Clear(context.Background()); err != nil {
+			t.Fatal("there should be no error:", err)
+		}
+		if err = store.Clear(ctx); err != nil {
+			t.Fatal("there should be no error:", err)
+		}
+	}()
+
+	// Run the actual test suite.
+	t.Log("event store with default namespace")
+	AcceptanceTest(t, context.Background(), store)
+
+	t.Log("event store with other namespace")
+	AcceptanceTest(t, context.Background(), store)
+
+	t.Log("event store maintainer")
+	testutil.MaintainerAcceptanceTest(t, context.Background(), store)
+}
